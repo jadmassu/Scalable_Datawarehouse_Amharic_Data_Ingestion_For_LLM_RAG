4bc140e415971f9987ba646dd8606ef54faa0eda
MoraaOntita <moraaontita1844@gmail.com>
2024-05-26 09:13:40 +0300

    Renamed the test_data_pipeline to test_data_collection and updated the code to test the updated dag

---
+++ .history/tests/test_data_collection_20240526091244.py
@@ -0,0 +1,28 @@
+import os
+import pytest
+from airflow.models import DagBag
+from airflow.utils.dates import days_ago
+
+# Define the path to your DAG file
+DAGS_FOLDER = os.path.join(os.path.dirname(__file__), '..', 'dags')
+
+@pytest.fixture(scope='module')
+def dagbag():
+    return DagBag(dag_folder=DAGS_FOLDER, include_examples=False)
+
+def test_dag_loaded_successfully(dagbag):
+    assert len(dagbag.dags) == 1, "Expected one DAG to be loaded"
+
+def test_data_collection_dag_structure(dagbag):
+    dag_id = 'data_collection'
+    dag = dagbag.get_dag(dag_id)
+    assert dag is not None, f"DAG '{dag_id}' not found"
+    
+    # Define expected task ids
+    expected_task_ids = ['scrape_news_task', 'save_to_db_task']
+
+    # Check if all expected tasks are present in the DAG
+    for task_id in expected_task_ids:
+        assert dag.has_task(task_id), f"Task '{task_id}' not found in DAG '{dag_id}'"
+
+
